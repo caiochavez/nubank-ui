964330fabf7920e4bfdd9d61e997f691967b90e3
caiochavez <caaiochavez@gmail.com>
2019-05-02 00:58:30 -0300

    Menu

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import {
   Container, Card, Content, CardHeader, CardContent, CardFooter, Title, Description, Annotation
 } from './styles'
 import Header from '~/components/header'
 import Tabs from '~/components/tabs'
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import Menu from '~/components/menu'
 
 export default function Main () {
   return (
     <Container>
       <Header />
       <Content>
+        <Menu />
         <Card>
           <CardHeader>
             <Icon name="attach-money" size={28} color="#666" />
@@ -18,11 +20,11 @@ export default function Main () {
           </CardHeader>
           <CardContent>
             <Title>Saldo disponível</Title>
-            <Description>R$ 8.000,00</Description>
+            <Description>R$ 100.000,00</Description>
           </CardContent>
           <CardFooter>
             <Annotation>
-              Transferência de R$ 1.000,00 recebida de Flávio Algusto hoje ás 08:00 horas
+              Transferência de R$ 1.000,00 recebida de Flávio Augusto hoje ás 08:00 horas
             </Annotation>
           </CardFooter>
         </Card>
